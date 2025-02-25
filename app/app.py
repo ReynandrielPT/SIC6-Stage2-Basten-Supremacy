c16af58262072376e06bc8e89c0bdc423a83f86d
ReynandrielPT <162027180+ReynandrielPT@users.noreply.github.com>
2025-02-25 10:01:40 +0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from bson import ObjectId  # Import ObjectId for conversion
+import os
 
 app = Flask(__name__)
 
@@ -24,4 +25,6 @@ def post_data():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
